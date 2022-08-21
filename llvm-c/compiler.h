f663d7b10e47333e723cf5509ab7d752d8e4ed81
redraiment <redraiment@gmail.com>
2022-08-21 15:50:00 +0800

    Close #10 lexical analysis with flex

--- llvm-c/compiler.h
+++ llvm-c/compiler.h
@@ -6,4 +6,11 @@
 void SetUpCompiler(void);
 void Compile(char*);
 
+void WhileNotZero(void);
+void WhileEnd(void);
+void MovePointer(int);
+void UpdateValue(int);
+void InputValue(void);
+void OutputValue(void);
+
 #endif
