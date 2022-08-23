af3db698455a04473125a8b841d62f238e2b2e51
redraiment <redraiment@gmail.com>
2022-08-23 14:10:01 +0800

    Fix #27 the link command length is difference in Ubuntu

--- linker.c
+++ linker.c
@@ -8,7 +8,11 @@
  * Invoke /bin/ld to link the object file to executable ELF file.
  */
 void Link(char* object, char* program) {
-  char command[100 + strlen(object) + strlen(program)];
+  char command[
+    30 + strlen(LINKER) + strlen(LOADER)
+    + strlen(C_RUNTIME_START) + strlen(C_RUNTIME_INIT) + strlen(C_RUNTIME_FINI)
+    + strlen(object) + strlen(program)
+  ];
   sprintf(command,
           "%s -dynamic-linker %s -lc %s %s %s %s -o %s",
           LINKER, LOADER,
